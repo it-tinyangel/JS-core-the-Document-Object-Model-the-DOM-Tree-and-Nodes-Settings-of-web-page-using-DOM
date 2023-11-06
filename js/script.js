abb85439322dde548469f8b7627089050afac6c8
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-11-06 12:29:34 +0200

    third commit
    since `document.p.getElementsByTagName('a')` method is not correct, I use document.querySelectorAll('#menu a') to get all links

--- js/script.js
+++ js/script.js
@@ -54,8 +54,7 @@ document.querySelector('#list').style.listStyleType = mainTypeListItemMarker;
 let userMenuLinks = prompt('Enter three web sites separated by commas, for example \"example1.com, example2.com, example3.com\":').split(', ');
 
 if (userMenuLinks.length === 3) {
-	// const userLinks = document.querySelectorAll('#menu a');
-	const userLinks = document.p.getElementsByTagName('a');
+	const userLinks = document.querySelectorAll('#menu a');
 		userMenuLinks.forEach((link, index) => {
 		userLinks[index].href = 'https://' + link;
 		userLinks[index].textContent = link;
