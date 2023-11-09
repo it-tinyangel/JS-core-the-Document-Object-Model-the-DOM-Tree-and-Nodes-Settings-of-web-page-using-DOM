5f260ab1e9947e7dfb2ef212c03e2887405b9d1c
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-11-10 01:12:37 +0200

    script.js
    Using function, functionality remained unchanged

--- js/script.js
+++ js/script.js
@@ -1,58 +1,81 @@
-// Get and set the background color for the page
-const bodyBackgroundColor = prompt('Select and enter the background color for the page, for example, tan, crimson, or steelblue:');
-document.body.style.backgroundColor = bodyBackgroundColor;
-
-// Get and set the font style for the page
-const bodyFontStyle = prompt('Select and enter the font style for the page (normal or italic):');
-document.body.style.fontStyle = bodyFontStyle;
-
-// Get and set alignment for heading h1
-const headerTextAlign = prompt('Select and enter alignment for the "My Page" header (left, right, or center):');
-document.querySelector('.header').style.textAlign = headerTextAlign;
-
-// Get and set the background color for the navigation with links
-const navBackgroundColor = prompt('Select and enter the background color of the "My Favorite Sites" navigation on the page, for example, peru, crimson, or brown:');
-const nav = document.getElementById('nav');
-nav.style.backgroundColor = navBackgroundColor;
-document.querySelector('hr').style.borderColor = navBackgroundColor;
-
-// Get and set the text color for the navigation with links
-const navTextColor = prompt('Select and enter a text color for the "My Favorite Sites" navigation on the page, for example, white or black:');
-nav.style.color = navTextColor;
-
-// Get and set the text color for the links
-const navLinks = document.querySelectorAll('.nav a');
-for (const link of navLinks) {
-	link.style.color = navTextColor;
+// Function to get user input for styles
+function getUserStyles(element, properties, promptMessage) {
+	const userStyles = {};
+	for (const property of properties) {
+		const userInput = prompt(`${promptMessage} (for ${property}):`);
+		userStyles[property] = userInput;
+	}
+	return userStyles;
 }
 
-// Get and set the text color for the main content
-const mainTextColor = prompt('Select and enter the text color for the main page, setting, for example, white or black:');
+// Function to set text color for navigation and links
+function setTextColor(nav, links, color) {
+	nav.style.color = color;
+	links.forEach(link => link.style.color = color);
+}
+
+// Function to set background color for navigation and hr borderColor
+function setNavBackgroundColor(nav, hr, color) {
+	nav.style.backgroundColor = color;
+	hr.style.borderColor = color;
+}
+
+// Function to set styles for the last paragraph in main content
+function setLastChildStyles(element, styles) {
+	const lastChild = element.querySelector('p:last-child');
+	if (lastChild) {
+		Object.assign(lastChild.style, styles);
+	}
+}
+
+// Function to get user input for navigation links
+function getUserNavLinks() {
+	const userInput = prompt('Enter three websites separated by commas, for example "example1.com, example2.com, example3.com":');
+	return userInput !== null ? userInput.split(', ').slice(0, 3) : null;
+}
+
+// Get and set styles for the page
+const body = document.body;
+const pageProperties = ['backgroundColor', 'fontStyle'];
+const pageStyles = getUserStyles(body, pageProperties, 'Enter styles for the page');
+Object.assign(body.style, pageStyles);
+
+// Get and set alignment for heading h1
+const header = document.querySelector('.header');
+const headerStyles = getUserStyles(header, ['textAlign'], 'Enter alignment for header');
+Object.assign(header.style, headerStyles);
+
+// Get and set styles for navigation links
+const nav = document.getElementById('nav');
+const hr = document.querySelector('hr');
+const navProperties = ['backgroundColor'];
+const navStyles = getUserStyles(nav, navProperties, 'Enter background color for navigation');
+setNavBackgroundColor(nav, hr, navStyles.backgroundColor);
+
+const navLinks = document.querySelectorAll('.nav a');
+const textColor = prompt('Enter text color for navigation and its links:');
+setTextColor(nav, navLinks, textColor);
+
+// Get and set styles for the main content
 const mainContent = document.querySelector('main');
-mainContent.style.color = mainTextColor;
-document.querySelector('section>p:nth-of-type(4)').style.color = mainTextColor;
+const mainProperties = ['color', 'fontSize', 'fontWeight'];
+const mainStyles = getUserStyles(mainContent, mainProperties, 'Enter styles for the main');
 
-// Get and set the font size for the main content
-const mainFontSize = prompt('Select and enter a font size for the main page, set to a size in pixels (px) or such as small, medium, large, or larger:');
-mainContent.style.fontSize = mainFontSize;
-
-// Get and set the font weight for the main content
-const mainFontWeight = prompt('Select and enter a font weight setting for the main page, choosing 400, 600, or 800, or normal or bold:');
-mainContent.style.fontWeight = mainFontWeight;
+// Set styles for the main content and its last paragraph
+Object.assign(mainContent.style, mainStyles);
+setLastChildStyles(mainContent, mainStyles);
 
 // Get and set the marker type for a list on the page
-const mainTypeListMarker = prompt('Select and enter the list marking type on the page, choosing disc, square, or circle:');
-document.querySelector('#list').style.listStyleType = mainTypeListMarker;
+const mainTypeListMarker = document.querySelector('#list');
+const listMarkerStyles = getUserStyles(mainTypeListMarker, ['listStyleType'], 'Enter styles for the list marker');
+Object.assign(mainTypeListMarker.style, listMarkerStyles);
 
-// Get and set links to three web sites entered by the user
-const userNavLinks = prompt('Enter three web sites separated by commas, for example "example1.com, example2.com, example3.com":').split(', ');
-
-if (userNavLinks.length === 3) {
+// Get and set user input for navigation links
+const userNavLinks = getUserNavLinks();
+if (userNavLinks !== null) {
 	const userLinks = document.querySelectorAll('#nav a');
 	userNavLinks.forEach((link, index) => {
 		userLinks[index].href = 'https://' + link;
 		userLinks[index].textContent = link;
 	});
-} else {
-	alert('You didn\'t enter three sites...');
 }
